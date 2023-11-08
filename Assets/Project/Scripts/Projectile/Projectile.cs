44ffee0947d2bcf03660605a548687674c3b3fe9
Shiro <eric.maestre@enti.cat>
2023-11-08 15:05:30 +0100

    I forgot to save and push the C Scripts

--- Assets/Project/Scripts/Projectile/Projectile.cs
+++ Assets/Project/Scripts/Projectile/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public abstract class Projectile : MonoBehaviour
 {
+
     float damage = 10f;
     public abstract void BulletMovement();
     protected virtual void OnCollisionEnter2D(Collision2D collision)
