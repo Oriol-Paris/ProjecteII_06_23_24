1e98ff27b4fec68f364bf99966bb36fb23c4be1d
Oriol-Paris <oriol.paris@enti.cat>
2023-11-23 16:32:02 +0100

    Just Started

---
+++ Assets/Project/Scripts/In Combat/Enemies/Enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    protected int health = 100;
+    [SerializeField]
+    protected int attack = 101;
+    [SerializeField]
+    protected int agility = 102;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
