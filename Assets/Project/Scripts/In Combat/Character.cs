b3ca21ad7cf8418b35b8eb7563993e109cfc7a24
Eric-Maestre <eric.maestre@enti.cat>
2023-12-11 15:36:07 +0100

    Stats fixed

--- Assets/Project/Scripts/In Combat/Character.cs
+++ Assets/Project/Scripts/In Combat/Character.cs
@@ -25,6 +25,7 @@ public abstract class Character : MonoBehaviour
     private float maxMana;
 
     protected bool isInmortal = false;
+    private bool defenseCalled = false;
 
     [SerializeField]
     protected int accuracy;
@@ -32,7 +33,10 @@ public abstract class Character : MonoBehaviour
 
     public SpriteRenderer sr;
 
-    private float defenseIncrease = 1.0f;
+    private float magicalDefense;
+    private float physicalDefense;
+
+    
 
     protected bool GetIsInmortal() { return isInmortal;}
 
@@ -40,7 +44,7 @@ public abstract class Character : MonoBehaviour
     //remember that increase is a percentage->%, it will reduce directly the atkVal by the percentage
     protected void DefenseUp(int increase)
     {
-        defenseIncrease = increase;
+        defenseCalled = true;
     }
 
     private void Awake()
@@ -49,26 +53,32 @@ public abstract class Character : MonoBehaviour
         health = maxHealth;
         maxMana = 1.5f * intelligence;
         mana = maxMana;
+        physicalDefense = resistance * 0.7f + strength * 0.3f;
+        magicalDefense = resistance * 0.7f + intelligence * 0.3f;
     }   
 
     protected virtual void PhysiqueDamage(Character other, int atkPow)
     {
         if (Random.Range(1, 100) < accuracy)
         {
-            float enemyDefense = other.resistance * 0.7f + other.strength * 0.3f;
-            float atkVal = 0.01f *Random.Range(85,100) * (((0.2f * level + 1) * strength * atkPow)/ (25.0f* enemyDefense) + 2) ;
+            float myDefense = other.physicalDefense;
+
+            if (defenseCalled)
+                other.physicalDefense = physicalDefense * 1.5f;
+
+            float atkVal = 0.01f *Random.Range(85,100) * (((0.2f * level + 1) * strength * atkPow)/ (25.0f* other.physicalDefense) + 2) ;
             if (Random.Range(1, 100) < luck * 1.25)
                 atkVal = atkVal * 1.5f;
-
-            if(defenseIncrease != 100)
-            {
-                atkVal = (defenseIncrease * atkVal / 100);
-                defenseIncrease = 100;
-            }
             other.health -= atkVal;
             string atkMessage = "Attack Connected! -> " + atkVal.ToString() + " damage";
 
             Debug.Log(atkMessage);
+
+            if(defenseCalled)
+            {
+                defenseCalled = false;
+                other.physicalDefense = myDefense;
+            }
 
             if (other.GetIsInmortal())
             {
@@ -90,13 +100,32 @@ public abstract class Character : MonoBehaviour
     {
         if (Random.Range(1, 100) < accuracy)
         {
-            float enemyDefense = other.resistance * 0.7f + other.intelligence * 0.3f;
-            float atkVal = 0.01f * Random.Range(85, 100) * (((0.2f * level + 1) * intelligence) / (25.0f * enemyDefense) + 2);
+
+            float myDefense = other.magicalDefense;
+
+            if(defenseCalled)
+                other.magicalDefense = magicalDefense * 1.5f;
+            
+            
+            float atkVal = 0.01f * Random.Range(85, 100) * (((0.2f * level + 1) * intelligence) / (25.0f * other.magicalDefense) + 2);
             if (Random.Range(1, 100) < luck * 1.25)
                 atkVal = atkVal * 1.5f;
             other.health -= atkVal;
             string atkMessage = "Attack Connected! -> " + atkVal.ToString() + " damage";
             Debug.Log(atkMessage);
+
+            if(defenseCalled)
+            {
+                defenseCalled = false;
+                other.magicalDefense = myDefense;
+            }
+
+            if (other.GetIsInmortal())
+            {
+                if (other.health <= 0)
+                    other.health = 1;
+                Debug.Log("Enemy is Inmortal");
+            }
         }
         else
         {
